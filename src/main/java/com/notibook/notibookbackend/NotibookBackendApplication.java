24f8583c1c6d042cef3384742b4c31a6c309036e
ericlee05 <develretr0@gmail.com>
2023-04-14 18:12:40 +0900

    Init commit

---
+++ src/main/java/com/notibook/notibookbackend/NotibookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.notibook.notibookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotibookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotibookBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/notibook/notibookbackend/NotibookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notibook.notibookbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotibookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
